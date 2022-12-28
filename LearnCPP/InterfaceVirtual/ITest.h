e96dcb0b1a700c66733c7aa99ffa3ca37ab926b5
bairutai <bairutai@aliyun.com>
2022-12-28 10:53:06 +0800

    1.提交C++抛出异常的代码

--- LearnCPP/InterfaceVirtual/ITest.h
+++ LearnCPP/InterfaceVirtual/ITest.h
@@ -4,6 +4,8 @@ class ITest
 public:
 	ITest();
 	~ITest();
+	virtual void add();
+
 	virtual void init();
 
 	virtual void test() = 0;
--- LearnCPP/LearnCPP/main.cpp
+++ LearnCPP/LearnCPP/main.cpp
@@ -1,4 +1,45 @@
+#include <iostream>
+using namespace std;
+void throwExpt()
+{
+	throw 1;
+}
+
+void noBlockThrow()
+{
+	throwExpt();
+}
+
+void blockThrow() noexcept
+{
+	throwExpt();
+}
+
 int main()
 {
+	try
+	{
+		throwExpt();
+	}
+	catch (...)
+	{
+		cout << "Found throw" << endl;
+	}
+	try
+	{
+		noBlockThrow();
+	}
+	catch (...)
+	{
+		cout << "throw is not block" << endl;
+	}
+	try
+	{
+		blockThrow();
+	}
+	catch (...)
+	{
+		cout << "throw is block" << endl;
+	} 
 	return 0;
 }
